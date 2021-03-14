f6897a70465e2c3f3e9984ab1690a2e731b4f6ac
Jean Machuca <correojean@gmail.com>
2021-03-14 19:42:57 -0300

    isolating component live-code

--- doc/js/packages/org.quickcorp.custom.components.js
+++ doc/js/packages/org.quickcorp.custom.components.js
@@ -28,5 +28,82 @@ Package("org.quickcorp.custom.components",[
     dependencies:[],
     name:'markdowncomponent',
     templateHandler: 'MarkdownTemplateHandler'
+  }),
+  Class ("LiveCodeComponent", Component, {
+    name:"live-code",
+    shadowed: false,
+    tplsource: "inline",
+    template: `
+    <style>
+      @import url('./doc/css/github-style.css');
+      @import url('./doc/css/snippet.css');
+
+        /* If you use shadowed=true
+        This style will be automatically shadowed in the browser */
+        div.container {
+          box-shadow: rgba(0, 0, 0, 0.2) 0px 1px 2px 0px, rgba(228, 228, 234, 0.5) 0px 9px 23px 0px;
+          transition: all 0.3s ease-in-out;
+          width: 100%;
+          margin: 0 auto;
+          min-height: 100%;
+          height: auto;
+          border-radius: 30px;
+          display: block;
+          padding: 3%;
+          background-color: #ffffff;
+        }
+
+
+        a{
+          /* These are technically the same, but use both */
+          overflow-wrap: break-word;
+          word-wrap: break-word;
+
+          -ms-word-break: break-all;
+          /* This is the dangerous one in WebKit, as it breaks things wherever */
+          word-break: break-all;
+          /* Instead use this non-standard one: */
+          word-break: break-word;
+
+          /* Adds a hyphen where the word breaks, if supported (No Blink) */
+          -ms-hyphens: auto;
+          -moz-hyphens: auto;
+          -webkit-hyphens: auto;
+          hyphens: auto;
+        }
+        p:not(:first-child) {
+          margin: 1% 5%;
+          font-size: medium;
+          display: block;
+        }
+        h1, h2 {
+          margin: 2% 3%;
+        }
+        ul {
+          margin: auto 4%;
+        }
+
+        .livecode_grid {
+            display: grid;
+            grid-template-rows: auto;
+            grid-template-columns: 50% 50%;
+        }
+    </style>
+    <div class="container">
+
+    <div class="livecode_grid" >
+      <component class="code_editor" name="{{editorname}}" componentClass="FormField" controllerClass="LiveCodeController">
+      </component>
+      <div class="code_result">
+        <p><button onclick="location.href='#result'" >RUN</button></p>
+        <p></p>
+      <iframe style="border:1px solid black; width:100%; height:70%" name="result" id="result" ></iframe>
+      </div>
+
+    </div>
+
+
+    </div>
+    `
   })
 ]);
